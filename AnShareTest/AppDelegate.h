acff6a94c9ab0482e3469c5b99ecd2140bd7157d
asdasdasd <362839344@qq.com>
2018-03-09 11:55:40 +0800

    Initial Commit

---
+++ AnShareTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnShareTest
+//
+//  Created by twh on 2018/3/9.
+//  Copyright © 2018年 362839344. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnShareTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnShareTest
+//
+//  Created by twh on 2018/3/9.
+//  Copyright © 2018年 362839344. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
